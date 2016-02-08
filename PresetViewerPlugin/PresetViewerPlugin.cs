253bcf187095c631d4b601ed31e5c5126b08b633
ruhiel <ruhiel1983@gmail.com>
2016-02-08 22:37:01 +0900

    KanColleViewer ver.4.2 対応

--- PresetViewerPlugin/PresetViewerPlugin.cs
+++ PresetViewerPlugin/PresetViewerPlugin.cs
@@ -8,7 +8,7 @@ namespace PresetViewerPlugin
     [ExportMetadata("Guid", "53F7734F-5CB8-47DB-9B7D-604A742FB1D6")]
     [ExportMetadata("Title", "PresetViewer")]
     [ExportMetadata("Description", "プリセット一覧を表示します。")]
-    [ExportMetadata("Version", "1.0.0")]
+    [ExportMetadata("Version", "1.0.1")]
     [ExportMetadata("Author", "@ruhiel_murrue")]
     class PresetViewerPlugin : ITool, IPlugin
     {
